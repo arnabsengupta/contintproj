27c4589ab10795898fe5dda4a5863bb6c26c4043
Arnab <sengarnab@gmail.com>
2017-08-30 13:40:00 +0530

    changes

--- SeleniumTest/src/test/com/arnab/Test2.java
+++ SeleniumTest/src/test/com/arnab/Test2.java
@@ -1,5 +1,32 @@
 package test.com.arnab;
 
-public class Test2 {
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
 
+import test.com.arnab.SeleniumTest.BaseTest;
+
+public class Test2 extends BaseTest{
+
+	@Before
+	public void setUp(){
+		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+		driver = new ChromeDriver();
+	}
+	@Test
+	public void test(){
+		
+		driver.get("http://www.calculator.net/");
+		driver.findElement(By.partialLinkText("Mortgage Calculator")).click();
+		driver.findElement(By.cssSelector("input[type=\"image\"]:nth-child(2)")).click();
+		
+		
+	}
+	
+	@After
+	public void quit(){
+		driver.quit();
+	}
 }
