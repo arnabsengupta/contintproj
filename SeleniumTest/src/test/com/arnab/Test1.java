722d313105e890c5605e9ec521a57a9218282f37
Arnab <sengarnab@gmail.com>
2017-09-01 20:15:11 +0530

    changes

--- SeleniumTest/src/test/com/arnab/Test1.java
+++ SeleniumTest/src/test/com/arnab/Test1.java
@@ -1,8 +1,12 @@
 package test.com.arnab;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.util.Properties;
 
 import test.com.arnab.SeleniumTest.BaseTest;
@@ -11,7 +15,12 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class Test1 extends BaseTest{
 
@@ -27,8 +36,19 @@ public String SEL_PROP = "selenium.properties";
 	
 	@Before
 	public void setUp(){
-		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
-		driver = new ChromeDriver();
+		System.setProperty("webdriver.chrome.driver", "/home/tamajit/Downloads/chromedriver");
+		//System.setProperty("webdriver.chrome.driver", "C:\\Users\\IBM_ADMIN\\mavenworkspace\\SeleniumTest\\chromedriver.exe");
+		//driver = new ChromeDriver();
+		DesiredCapabilities capability = DesiredCapabilities.chrome();
+		//capability.setCapability(ChromeOptions.CAPABILITY, 0);
+	
+		try {
+			driver = new RemoteWebDriver(new URL("http://9.184.214.122:5558/wd/hub"), DesiredCapabilities.chrome());
+			//driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"),capability);
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	@Test
 	public void test(){
